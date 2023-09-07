73d1110318f7b9619ca92bc57b242eb38bcf8ee9
scary sources <scarysources@tutanota.org>
2023-09-07 20:22:19 +0300

    patch configuration

--- lisum_bot/config.py
+++ lisum_bot/config.py
@@ -2,54 +2,35 @@
 This file contain validation template for
 bot's configuratin, given from environ.
 """
-from traceback import print_exception
 
-import environ
-from attr.validators import matches_re, optional
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN_RE = "[0-9]{8,10}:[a-zA-Z0-9_-]{35}"
-REDIS_URL_RE = "redis://.+:[0-9]+/.+"
-HOSTNAME_RE = ".+"
-PORT_RE = "[0-9]+"
-
-vault = environ.secrets.VaultEnvSecrets(vault_prefix="BOT")
+from loguru import logger
+from pydantic import (
+    AnyHttpUrl,
+    BaseModel,
+    Field,
+    RedisDsn,
+    ValidationError,
+)
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-@environ.config(prefix="")
-class AppConfig:
-    debug = environ.bool_var(help="Redis url", default=False)
+class BotConfig(BaseModel):
+    token: str = Field(pattern="^[0-9]{8,10}:[a-zA-Z0-9_-]{35}$")
 
-    @environ.config(prefix="BOT")
-    class BotConfig:
-        token = vault.secret(help="Telegram bot's token")
 
-    @environ.config(prefix="REDIS")
-    class RedisConfig:
-        url = environ.var(
-            help="Redis url", validator=optional(matches_re(REDIS_URL_RE)), default=None
-        )
-        host = environ.var(
-            help="Redis host", validator=optional(matches_re(HOSTNAME_RE)), default=None
-        )
-        port = environ.var(
-            help="Redis port", validator=optional(matches_re(PORT_RE)), default=None
-        )
+class LisumConfig(BaseModel):
+    url: AnyHttpUrl
 
-    @environ.config(prefix="OPENAI")
-    class OpenAiConfig:
-        api_key = environ.var(help="OpenAI token", default=None)
 
-    bot = environ.group(BotConfig)
-    redis = environ.group(RedisConfig)
-    open_ai = environ.group(OpenAiConfig)
+class Settings(BaseSettings):
+    bot: BotConfig
+    redis: RedisDsn = Field('redis://localhost:6379/db')
+    lisum: LisumConfig
 
+    model_config = SettingsConfigDict(env_nested_delimiter='_', env_file='.env')
 
 try:
-    config = environ.to_config(AppConfig)
-except Exception as e:
-    print_exception(e)
-    print("\n", environ.generate_help(AppConfig), "\n")
-    quit()
+    config = Settings()
+except ValidationError as exc:
+    logger.error(exc)
+    quit(1)
--- lisum_bot/loader.py
+++ lisum_bot/loader.py
@@ -2,38 +2,36 @@
 This file contain all global objects for bit,
 to avoid circular imports.
 """
+import os
+
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.base import BaseStorage
-from loguru import logger
 from aiogram.fsm.storage.memory import MemoryStorage
-import os
-# from lisum_bot.config import config
+from loguru import logger
 
-# # if config.TELEGRAM_TEST_SERVER:
-# #     bot.server = TELEGRAM_TEST
-# # TODO test server
+from lisum_bot.config import config
 
-# storage: BaseStorage
-# if config.redis.host and config.redis.port:
-#     from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
-#     from redis.asyncio import Redis
+storage: BaseStorage
+if config.redis.host and config.redis.port:
+    from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
+    from redis.asyncio import Redis
 
-#     storage = RedisStorage(
-#         Redis(host=config.redis.host, port=config.redis.port),
-#         key_builder=DefaultKeyBuilder(with_destiny=True),
-#     )
-#     logger.info(f"redis host: {config.redis.host}, redis port: {config.redis.port}")
-# elif config.redis.url:
-#     from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
+    storage = RedisStorage(
+        Redis(host=config.redis.host, port=config.redis.port),
+        key_builder=DefaultKeyBuilder(with_destiny=True),
+    )
+    logger.info(f"redis host: {config.redis.host}, redis port: {config.redis.port}")
+elif config.redis.url:
+    from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
 
-#     storage = RedisStorage.from_url(
-#         config.redis.url, key_builder=DefaultKeyBuilder(with_destiny=True)
-#     )
-#     logger.info(f"redis url: {config.redis.url}")
-# else:
-#     from aiogram.fsm.storage.memory import MemoryStorage
+    storage = RedisStorage.from_url(
+        config.redis.url, key_builder=DefaultKeyBuilder(with_destiny=True)
+    )
+    logger.info(f"redis url: {config.redis.url}")
+else:
+    from aiogram.fsm.storage.memory import MemoryStorage
 
-#     storage = MemoryStorage()
+    storage = MemoryStorage()
 
-bot = Bot(token=os.getenv("BOT_TOKEN"))
+bot = Bot(token=config.bot.token)
 dp = Dispatcher(storage=MemoryStorage())
